cc0bf970c2cdb60b6039aa956dc5d9475ef4368c
hsushipei <hsushipei1@gmail.com>
2016-11-28 13:42:44 +0800

    Initialization

---
+++ website_tools/gitpush_after_imgupd.bash
@@ -0,0 +1,10 @@
+# This program pushes latest file to Github after things are updated.
+upd_t=$(date +%Y-%-m-%d-%H) # current time
+cmt_msg="Weather map update commit at "$upd_t # message for images update commit
+alias git="~/miniconda2/bin/git"
+
+echo "===== start -> gitpush_after_imgupd.bash ===="  
+git add .
+git commit -m "Weather map update commit"
+git push -f -u origin master
+echo "===== gitpush_after_imgupd.bash -> end ===="
