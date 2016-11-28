76793cc3336ca87439439a56aa991fdd5b32cb40
hsushipei <hsushipei1@gmail.com>
2016-11-28 19:46:22 +0800

    Weather map update commit

--- website_tools/gitpush_after_imgupd.bash
+++ website_tools/gitpush_after_imgupd.bash
@@ -1,6 +1,4 @@
 # This program pushes latest file to Github after things are updated.
-upd_t=$(date +%Y-%-m-%d-%H) # current time
-cmt_msg="Weather map update commit at "$upd_t # message for images update commit
 alias git="~/miniconda2/bin/git"
 
 echo "===== start -> gitpush_after_imgupd.bash ===="  
