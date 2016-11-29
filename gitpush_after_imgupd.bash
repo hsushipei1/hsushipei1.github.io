dfe21dd88c1d975e2029f50881b0244a6082f4ea
hsushipei <hsushipei1@gmail.com>
2016-11-29 14:13:01 +0800

    Weather map update commit

--- gitpush_after_imgupd.bash
+++ gitpush_after_imgupd.bash
@@ -1,8 +1,6 @@
 # This program pushes latest file to Github after things are updated.
-alias git="~/miniconda2/bin/git"
-
 echo "===== start -> gitpush_after_imgupd.bash ===="  
-git add --all
-git commit -m "Weather map update commit"
-git push -u origin master
+/home/hsushipei/miniconda2/bin/git add --all
+/home/hsushipei/miniconda2/bin/git commit -m "Weather map update commit"
+/home/hsushipei/miniconda2/bin/git push -u origin master
 echo "===== gitpush_after_imgupd.bash -> end ===="
