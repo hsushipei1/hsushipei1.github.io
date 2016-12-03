dae2b4a22265faa0c19e72d2531a2c51296bd6e5
hsushipei1 <hsushipei1@gmail.com>
2016-12-03 21:54:42 +0800

    practice JS

---
+++ practice/myscript_001.js
@@ -0,0 +1,12 @@
+var clickbtn = function() {
+	document.getElementById("hello").innerHTML = "I'm Jack!"
+	console.log("Click!");
+}
+
+var EAIR_btn_show = function() {
+	document.getElementById("satel").src = "http://opendata.cwb.gov.tw/opendata/MSC/O-B0030-002.jpg";
+}
+
+var EAVIS_btn_show = function() {
+	document.getElementById("satel").src = "http://opendata.cwb.gov.tw/opendata/MSC/O-B0031-002.jpg";
+}
